1d09dd1ef5983ff2b09d35ec1e08d89a1bc5c522
dannyk-dev <kruger.dkk@gmail.com>
2024-05-24 20:05:36 -0300

    Added datatables extensions in the npm registry

--- src/js/components/shared/app-user-list.js
+++ src/js/components/shared/app-user-list.js
@@ -3,6 +3,10 @@
 import DataTable from "datatables.net-dt";
 import { getUsers } from "../../services/users";
 import Events from "../../utils/Events";
+import "datatables.net-buttons-bs5";
+import "datatables.net-fixedcolumns-bs5";
+import "jquery-datatables-checkboxes";
+import "datatables.net-fixedheader-bs5";
 
 // $(function () {
 //   let borderColor, bodyBg, headingColor;
@@ -110,6 +114,78 @@ import Events from "../../utils/Events";
 //   });
 // });
 
+function renderAvatar(fullName, avatar) {
+  let avatarHtml = "";
+
+  if (avatar) {
+    avatarHtml = `<img src="${assetsPath}/assets/img/avatars/${avatar}" alt="Avatar" class="rounded-circle">`;
+  } else {
+    const initials = (fullName.match(/\b\w/g) || []).shift() || "";
+    avatarHtml = `<span class="avatar-initial rounded-circle bg-label-${
+      ["success", "danger", "warning", "info", "dark", "primary", "secondary"][
+        Math.floor(6 * Math.random())
+      ]
+    }">${initials.toUpperCase()}</span>`;
+  }
+
+  return `
+      <div class="d-flex justify-content-start align-items-center user-name">
+        <div class="avatar-wrapper">
+          <div class="avatar avatar-sm me-3">
+            ${avatarHtml}
+          </div>
+        </div>
+        <div class="d-flex flex-column">
+          <a href="${"#"}" class="text-body text-truncate">
+            <span class="fw-medium">${fullName}</span>
+          </a>
+        </div>
+      </div>
+    `;
+}
+
+function renderRole(role) {
+  return `
+      <span class='text-truncate d-flex align-items-center'>
+        ${
+          {
+            COLABORADOR: `<span class="badge badge-center rounded-pill bg-label-warning w-px-30 h-px-30 me-2"><i class="bx bx-user bx-xs"></i></span>`,
+            ADMIN: `<span class="badge badge-center rounded-pill bg-label-secondary w-px-30 h-px-30 me-2"><i class="bx bx-mobile-alt bx-xs"></i></span>`,
+          }[role]
+        }
+        ${role}
+      </span>
+    `;
+}
+
+function renderCPF(cpf) {
+  return `
+     <span class='text-truncate d-flex align-items-center'>
+      <span class="fw-medium ">${cpf}</span>
+    </span>`;
+}
+
+function renderPin(pin) {
+  return `
+  <span class='text-truncate d-flex align-items-center'>
+    <span class="fw-medium ">${pin}</span> 
+  </span>`;
+}
+
+function renderActionButtons() {
+  return `
+      <div class="d-inline-block text-nowrap">
+        <button class="btn btn-sm btn-icon"><i class="bx bx-edit"></i></button>
+        <button class="btn btn-sm btn-icon delete-record"><i class="bx bx-trash"></i></button>
+        <button class="btn btn-sm btn-icon dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="bx bx-dots-vertical-rounded me-2"></i></button>
+        <div class="dropdown-menu dropdown-menu-end m-0">
+          <a href="${"#"}" class="dropdown-item">View</a>
+          <a href="javascript:;" class="dropdown-item">Suspend</a>
+        </div>
+      </div>
+    `;
+}
+
 Events.$onPageLoad(async () => {
   // const statusLabels = {
   //   1: { title: "Na hora", class: "bg-label-success" },
@@ -119,7 +195,6 @@ Events.$onPageLoad(async () => {
 
   const usersTable = document.querySelector(".datatables-users");
   const { data } = await getUsers();
-  console.log(data);
 
   if (usersTable && data) {
     new DataTable(usersTable, {
@@ -148,39 +223,9 @@ Events.$onPageLoad(async () => {
           responsivePriority: 4,
           render: function (data, type, row, meta) {
             const fullName = row.username ?? "John Doe";
-            // const email = row.email;
             const avatar = row.avatar;
-            let avatarHtml = "";
-            if (avatar) {
-              avatarHtml = `<img src="${assetsPath}/assets/img/avatars/${avatar}" alt="Avatar" class="rounded-circle">`;
-            } else {
-              const initials = (fullName.match(/\b\w/g) || []).shift() || "";
-              avatarHtml = `<span class="avatar-initial rounded-circle bg-label-${
-                [
-                  "success",
-                  "danger",
-                  "warning",
-                  "info",
-                  "dark",
-                  "primary",
-                  "secondary",
-                ][Math.floor(6 * Math.random())]
-              }">${initials.toUpperCase()}</span>`;
-            }
-            return `
-              <div class="d-flex justify-content-start align-items-center user-name">
-                <div class="avatar-wrapper">
-                  <div class="avatar avatar-sm me-3">
-                    ${avatarHtml}
-                  </div>
-                </div>
-                <div class="d-flex flex-column">
-                  <a href="${"#"}" class="text-body text-truncate">
-                    <span class="fw-medium">${fullName}</span>
-                  </a>
-                </div>
-              </div>
-            `;
+
+            return renderAvatar(fullName, avatar);
           },
         },
 
@@ -189,17 +234,7 @@ Events.$onPageLoad(async () => {
           render: function (data, type, row, meta) {
             const role = row.role;
 
-            return `
-              <span class='text-truncate d-flex align-items-center'>
-                ${
-                  {
-                    COLABORADOR: `<span class="badge badge-center rounded-pill bg-label-warning w-px-30 h-px-30 me-2"><i class="bx bx-user bx-xs"></i></span>`,
-                    ADMIN: `<span class="badge badge-center rounded-pill bg-label-secondary w-px-30 h-px-30 me-2"><i class="bx bx-mobile-alt bx-xs"></i></span>`,
-                  }[role]
-                }
-                ${role}
-              </span>
-            `;
+            return renderRole(role);
           },
         },
         {
@@ -207,19 +242,15 @@ Events.$onPageLoad(async () => {
           render: function (data, type, row, meta) {
             const cpf = row.cpf ?? "No CPF Registered";
 
-            return `<span class='text-truncate d-flex align-items-center'>
-              <span class="fw-medium ">${cpf}</span>
-            </span>`;
+            return renderCPF(cpf);
           },
         },
         {
           targets: 4,
           render: function (data, type, row, meta) {
-            return `<span class='text-truncate d-flex align-items-center'>
-              <span class="fw-medium ">${
-                row.pin ?? "No Pin registered..."
-              }</span> 
-            </span>`;
+            const pin = row.pin ?? "No Pin registered...";
+
+            return renderPin(pin);
           },
         },
         {
@@ -228,17 +259,7 @@ Events.$onPageLoad(async () => {
           searchable: false,
           orderable: false,
           render: function (data, type, row, meta) {
-            return `
-              <div class="d-inline-block text-nowrap">
-                <button class="btn btn-sm btn-icon"><i class="bx bx-edit"></i></button>
-                <button class="btn btn-sm btn-icon delete-record"><i class="bx bx-trash"></i></button>
-                <button class="btn btn-sm btn-icon dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="bx bx-dots-vertical-rounded me-2"></i></button>
-                <div class="dropdown-menu dropdown-menu-end m-0">
-                  <a href="${"#"}" class="dropdown-item">View</a>
-                  <a href="javascript:;" class="dropdown-item">Suspend</a>
-                </div>
-              </div>
-            `;
+            return renderActionButtons();
           },
         },
       ],
@@ -249,53 +270,42 @@ Events.$onPageLoad(async () => {
         search: "",
         searchPlaceholder: "Pesquisa ",
       },
+
       buttons: [
         {
           extend: "collection",
           className: "btn btn-label-secondary dropdown-toggle mx-3",
           text: '<i class="bx bx-export me-1"></i>Export',
           buttons: [
-            {
-              extend: "print",
-              text: '<i class="bx bx-printer me-2" ></i>Print',
-              className: "dropdown-item",
-              exportOptions: {
-                columns: [1, 2, 3, 4, 5],
-                format: {
-                  body: function (data, row, column, node) {
-                    let formattedData = "";
-                    if (data.length > 0) {
-                      const parsedHtml = $.parseHTML(data);
-                      formattedData = $.map(parsedHtml, function (element) {
-                        if (
-                          element.classList &&
-                          element.classList.contains("user-name")
-                        ) {
-                          return element.lastChild.firstChild.textContent;
-                        } else if (element.innerText) {
-                          return element.innerText;
-                        } else {
-                          return element.textContent;
-                        }
-                      }).join("");
-                    }
-                    return formattedData;
-                  },
-                },
-              },
-              customize: function (win) {
-                $(win.document.body)
-                  .css("color", headingColor)
-                  .css("border-color", borderColor)
-                  .css("background-color", bodyBg);
-                $(win.document.body)
-                  .find("table")
-                  .addClass("compact")
-                  .css("color", "inherit")
-                  .css("border-color", "inherit")
-                  .css("background-color", "inherit");
-              },
-            },
+            // {
+            //   extend: "print",
+            //   text: '<i class="bx bx-printer me-2" ></i>Print',
+            //   className: "dropdown-item",
+            //   exportOptions: {
+            //     columns: [1, 2, 3, 4, 5],
+            //     format: {
+            //       body: function (data, row, column, node) {
+            //         let formattedData = "";
+            //         if (data.length > 0) {
+            //           const parsedHtml = $.parseHTML(data);
+            //           formattedData = $.map(parsedHtml, function (element) {
+            //             if (
+            //               element.classList &&
+            //               element.classList.contains("user-name")
+            //             ) {
+            //               return element.lastChild.firstChild.textContent;
+            //             } else if (element.innerText) {
+            //               return element.innerText;
+            //             } else {
+            //               return element.textContent;
+            //             }
+            //           }).join("");
+            //         }
+            //         return formattedData;
+            //       },
+            //     },
+            //   },
+            // },
             {
               extend: "csv",
               text: '<i class="bx bx-file me-2" ></i>Csv',
